9ef3024bce9e919235199497c117b55ef4a00ae1
Lee Cường <64338872+ryuuwon@users.noreply.github.com>
2021-09-25 02:34:56 +0700

    Delete controler.js

--- controler.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementsByClassName('open')[0].addEventListener('click', function () {
-    document.getElementsByClassName('card-packaging')[0].classList.add('is-open')
-  })
-
-  document.getElementsByClassName('close')[0].addEventListener('click', function () {
-    document.getElementsByClassName('card-packaging')[0].classList.remove('is-open')
-  })
-});
